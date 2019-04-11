cb5560542233821186a0b7b69f96e68128c11774
Marko Man <darkobas@gmail.com>
2019-04-11 14:32:14 +0200

    fod: differentiate dreaming and pulse
    add pulse callback, change icons accordingly
    
    Change-Id: I76758379059b846c187a7efe11835382872074e0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5490,6 +5490,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                         setPulsing(true);
                     }
                     setOnPulseEvent(reason, true);
+                    KeyguardUpdateMonitor.getInstance(mContext).setPulsing(true);
                 }
 
                 @Override
@@ -5497,6 +5498,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                     callback.onPulseFinished();
                     setPulsing(false);
                     setOnPulseEvent(-1, false);
+                    KeyguardUpdateMonitor.getInstance(mContext).setPulsing(false);
                 }
 
                 private void setPulsing(boolean pulsing) {
